8f8e20e58ddab02cf87f155e76aee6fa3d694367
Matt Weems <mj_weems@yahoo.com>
2015-08-06 16:08:01 +0100

    add second url

--- superlists/urls.py
+++ superlists/urls.py
@@ -18,6 +18,7 @@ from lists import views
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home')
+    url(r'^$', views.home_page, name='home'),
+    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list')
     # url(r'^admin/', include(admin.site.urls)),
 ]
