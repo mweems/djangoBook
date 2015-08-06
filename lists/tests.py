407c481370def041a18c7260f57efd14f8642571
Matt Weems <mj_weems@yahoo.com>
2015-08-06 13:59:34 +0100

    redirect after post, test cleanup

--- lists/tests.py
+++ lists/tests.py
@@ -22,12 +22,18 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         request.method = 'POST'
         request.POST['item_text'] = 'A new list item'
-
-        response = home_page(request)
+        home_page(request)
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
+
+    def test_home_page_redirects_after_post(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
